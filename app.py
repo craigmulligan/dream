58b9fed00db0218bea409d8831d58ddbcb287ba7
Craig Mulligan <hey@hobochild.com>
2021-06-08 09:05:23 +0200

    Add lambda handler

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
+import logging
+import json
+from time import sleep
 from chalice import Chalice, Response
 from chalice.test import Client
 
 app = Chalice(app_name="dream")
+# Enable DEBUG logs.
+app.log.setLevel(logging.DEBUG)
 
 
 @app.route("/xyz")
@@ -16,22 +21,26 @@ def index():
 
 @app.on_sns_message(topic="audit-log")
 def handle_sns_message(event):
+    sleep(10)
     app.log.debug(
         "Received message with subject: %s, message: %s", event.subject, event.message
     )
+    return {"hello": "world"}
 
 
 @app.route(
     "/{api_version}/functions/{function_name}/invocations",
-    methods=["GET", "POST"],
+    methods=["POST"],
     content_types=["application/x-amz-json-1.0"],
 )
 def proxy(api_version, function_name):
     print("local lambda proxy", api_version, function_name)
     print(app.current_request.raw_body)
     print(app.current_request.headers)
-    #  with Client(app) as client:
-    #  res = client.http.get("/xyz")
 
-    return {"hello": "world"}
-    # return Response(res.body, status_code=res.status_code, headers=res.headers)
+    event = json.loads(app.current_request.raw_body)
+
+    with Client(app) as client:
+        res = client.lambda_.invoke("handle_sns_message", event)
+
+        return Response(res.payload, status_code=200)
