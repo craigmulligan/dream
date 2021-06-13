9ba672e7674bb27e5b2eb4acb27a4e12fd244891
Craig Mulligan <hey@hobochild.com>
2021-06-13 15:40:29 +0200

    move to chalicelib

---
+++ chalicelib/api/user.py
@@ -0,0 +1,17 @@
+from chalice import Blueprint
+from chalicelib.models import User
+from chalicelib.schemas import UserSchema
+from chalicelib.database import session_scope
+
+blueprint = Blueprint(__name__)
+
+user_schema = UserSchema()
+
+
+@blueprint.route("/{user_id}", methods=["GET"])
+def get_user(user_id):
+
+    with session_scope() as session:
+        user = session.query(User).filter_by(id=user_id).first()
+
+        return user_schema.dump(user)
