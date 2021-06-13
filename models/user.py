572969b3cd5954dd35bdd348fc22b8fa4868a993
Craig Mulligan <hey@hobochild.com>
2021-06-13 15:40:24 +0200

    move to chalicelib

--- models/user.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, DateTime, Integer, String
-from .base import Base
-
-
-class User(Base):
-    __tablename__ = "users"
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    name = Column(String(), default=None)
-    created_at = Column(DateTime, default=None)
