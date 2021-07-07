46d6bb03b96164304b3a8335768a413d8e1f3fa4
Craig Mulligan <hey@hobochild.com>
2021-07-07 17:52:42 +0200

    fixup

--- chalicelib/api/auth.py
+++ chalicelib/api/auth.py
@@ -1,15 +1,33 @@
 from chalice import Blueprint, Response
+from requests_toolbelt import MultipartDecoder
+
 from chalicelib.database import session_scope
 from chalicelib.templates import templates
+from chalicelib.schemas import UserSchema
 
 blueprint = Blueprint(__name__)
+user_schema = UserSchema()
 
 
 @blueprint.route("/signin", methods=["GET"])
-def signin():
+def signin_get():
     template = templates.get_template("signin.html")
 
-    # request = blueprint.current_request
-    return Response(body=template.render(),
+    return Response(body=template.render(template),
+                    status_code=200,
+                    headers={'Content-Type': 'text/html'})
+
+
+@blueprint.route("/signin", methods=["POST"], content_types=['multipart/form-data'])
+def signin_post():
+    template = templates.get_template("home.html")
+
+    request = blueprint.current_request
+    decoder = MultipartDecoder(request.raw_body, request.headers['content-type'])
+
+    # TODO validate
+    user = user_schema.load(decoder.parts)
+     
+    return Response(body=template.render(template, email=user.email),
                     status_code=200,
                     headers={'Content-Type': 'text/html'})
--- tests/api/test_auth.py
+++ tests/api/test_auth.py
@@ -2,20 +2,22 @@ from chalicelib.models import User
 from chalicelib.templates import templates
 import json
 
-#  def test_signin(test_client, session):
-    #  assert session.query(User).count() == 0
-    #  password = "1234"
-    #  user = User(email="x@x.com", password=password)
-    #  session.add(user)
-    #  session.commit()
-
-    #  body = json.dumps({"id": user.id, "password": password }
-    #  response = test_client.http.post(f"/auth/signin")
-    #  assert response.status_code == 200 
-
 
 def test_get_signin(test_client):
+    # TODO should redirect
     template = templates.get_template("signin.html")
     response = test_client.http.get(f"/auth/signin")
     assert response.status_code == 200
     assert response.body.decode("utf-8") == template.render()
+
+
+def test_signin_post(test_client, session):
+    assert session.query(User).count() == 0
+    password = "1234"
+    user = User(email="x@x.com", password=password)
+    session.add(user)
+    session.commit()
+
+    # body = json.dumps({"id": user.id, "password": password })
+    #  response = test_client.http.post(f"/auth/signin")
+    #  assert response.status_code == 200
