e2c2793f527f07d41f5c46308c57fb66eb277da2
Craig Mulligan <hey@hobochild.com>
2021-12-08 12:23:57 +0200

    Invoke

--- tasks.py
+++ tasks.py
@@ -1,10 +1,18 @@
 from invoke import task
 
 @task
+def db(c):
+    c.run(f"docker-compose up -d", pty=True)
+
+@task
 def test(c, watch=False):
+    db(c)
     if watch:
         c.run(f"ptw -- --testmon", pty=True)
         return
     
     
     c.run(f"pytest")
+
+
+
