e42a2140fdf980517de5a23c439b85ea523ea22a
Craig Mulligan <hey@hobochild.com>
2021-12-10 18:20:18 +0200

    fix celery

--- app/tasks/audit.py
+++ app/tasks/audit.py
@@ -4,6 +4,5 @@ from app.database import celery
 
 @celery.task
 def audit_log(event_name, user_id):
-    print(f"New Event: {event_name}, {user_id}")
     logging.info(f"New Event: {event_name}, {user_id}") 
     return
