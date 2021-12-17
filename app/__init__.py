f46cdb1028140b122a306ad7b295dfc6bba7cecd
Craig Mulligan <hey@hobochild.com>
2021-12-17 17:22:50 +0200

    more mypyt

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,17 @@
+from typing import Type
 from flask import Flask
+from app.database.flask_celery import FlaskCelery
 
 from app.api import register_blueprints
 from app.database import db, celery
 from app.cli import register_cli
 
+class FlaskApp(Flask):
+    """Custom flask app with extensions"""
+    celery: Type[FlaskCelery]
 
 def create_app():
-    app = Flask(__name__)
+    app = FlaskApp(__name__)
     app.config.from_object("config")
 
     db.init_app(app)
