f6d3035e80c3d5f50036bfe967d8d2745a51b61c
Craig Mulligan <hey@hobochild.com>
2021-12-21 18:27:54 +0200

    Add basic session management

--- app/api/user.py
+++ app/api/user.py
@@ -1,11 +1,13 @@
 from flask import Blueprint, render_template, abort
 from app.models import User
 from app.tasks.audit import audit_log
+from app.api.utils import authenticated_resource
 
 blueprint = Blueprint("user", __name__)
 
 
 @blueprint.route("/<int:user_id>", methods=["GET"])
+@authenticated_resource
 def get_user(user_id):
     audit_log.delay("user_view", user_id)
     user = User.query.filter_by(id=user_id).one_or_none()
