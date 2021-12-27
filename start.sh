4d66befa9ecd10af568f8095249d95acd0213e9f
Craig Mulligan <hey@hobochild.com>
2021-12-27 18:21:31 +0200

    fly

--- start.sh
+++ start.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 set -e
-
-
-set -e
 set -m # to make job control work
-flask db upgrade &&
-gunicorn "run_app:app" -b 0.0.0.0:8080 &
-celery --app "run_app:celery" worker
+
+flask db upgrade && gunicorn "run_app:app" -b 0.0.0.0:8080 & celery --app "run_app:celery" worker
 fg %1
