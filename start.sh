7e39e84ff027c87e83a1ec3d1d795108957bd220
Craig Mulligan <hey@hobochild.com>
2021-12-28 18:18:59 +0200

    add release command

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
+# TODO user supervisor instead.
 #!/bin/bash
 set -e
 set -m # to make job control work
 
-flask db upgrade && gunicorn "run_app:app" -b 0.0.0.0:8080 & celery --app "run_app:celery" worker
+gunicorn "run_app:app" -b 0.0.0.0:8080 & celery --app "run_app:celery" worker
 fg %1
