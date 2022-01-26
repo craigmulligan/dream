f62be7ed339f93af41cf501147e4dcc0ed482150
Craig Mulligan <hey@hobochild.com>
2022-01-26 16:55:54 +0000

    fixup flask.current_app typing

--- app/tasks/email.py
+++ app/tasks/email.py
@@ -1,8 +1,10 @@
 import logging
 from app.database import celery
 from flask import current_app
+from app import FlaskApp
+from typing import cast
 
 
 @celery.task
 def email_send(to, subject, body):
-    return current_app.mail_manager.send(to, subject, body)
+    return cast(FlaskApp, current_app).mail_manager.send(to, subject, body)
